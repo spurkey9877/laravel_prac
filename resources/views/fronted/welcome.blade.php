89372bf57f41b26bbcc5b2e1dfd5318aeabee582
spurkey9877 <spurkey9877@gmail.com>
2020-08-09 23:30:40 +0800

    Add frontedlayouts index part

--- resources/views/fronted/welcome.blade.php
+++ resources/views/fronted/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel YOOOOOO
+                    Laravel HI! {{ $name }}
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.welcome',['name'=>'Frank']);
 });
+
+Route::get('/index',function(){
+    return view('fronted.index');
+})->name('home');
